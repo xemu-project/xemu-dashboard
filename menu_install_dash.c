0a44f7c729712184417f32adadb1fc0f90d7f222
Ryzee119 <wendland@live.com.au>
2025-07-11 16:49:37 +0930

    installer: Clean up offline and online options

--- menu_install_dash.c
+++ menu_install_dash.c
@@ -5,38 +5,8 @@
 #include <xboxkrnl/xboxkrnl.h>
 
 #include "main.h"
-static void check_online(void);
-
-static void install_dashboard(void)
-{
-    static MenuItem status_message_items[] = {
-        {"Installed successfully", NULL},
-        {"Error installing", NULL}};
-
-    static Menu status_message = {
-        .item = NULL,
-        .item_count = 1,
-        .selected_index = 0,
-        .scroll_offset = 0};
-
-    const char *src = "Q:\\default.xbe";
-    const char *dst = "C:\\xboxdash.xbe";
-    const char *bak = "C:\\xboxdash.xbe.bak";
-
-    SetFileAttributesA(dst, FILE_ATTRIBUTE_NORMAL);
-    SetFileAttributesA(bak, FILE_ATTRIBUTE_NORMAL);
-
-    // Create a backup
-    CopyFile(dst, bak, FALSE);
-
-    if (CopyFile(src, dst, FALSE)) {
-        status_message.item = &status_message_items[0];
-        menu_push(&status_message);
-    } else {
-        status_message.item = &status_message_items[1];
-        menu_push(&status_message);
-    }
-}
+static void install_dashboard_from_online(void);
+static void install_dashboard_from_local(void);
 
 static void restore_backuo(void)
 {
@@ -68,11 +38,17 @@ static void cancel(void)
     menu_pop();
 }
 
+// These text fields dynamically change so we use a separate variable for them
+static char *default_offline_install_text = "Install running file? (default.xbe)";
+static char *default_online_install_text = "Check online for latest file?";
+#define OFFLINE_INSTALL_LINE 2
+#define ONLINE_INSTALL_LINE 3
+
 static MenuItem menu_items[] = {
-    {"Dashboard Installer", NULL},
+    {"Dashboard Installer (This will replace xboxdash.xbe)", NULL},
     {"Restore backup", restore_backuo},
-    {"Install and overwrite \"C:\\xboxdash.xbe\"?", install_dashboard},
-    {"Check online for updates", check_online},
+    {"Install running file?", install_dashboard_from_local},
+    {"Check online for latest file?", install_dashboard_from_online},
     {"Cancel", cancel}};
 
 static Menu menu = {
@@ -83,12 +59,14 @@ static Menu menu = {
 
 void menu_install_dash_activate(void)
 {
+    menu_items[OFFLINE_INSTALL_LINE].label = default_offline_install_text;
+    menu_items[ONLINE_INSTALL_LINE].label = default_online_install_text;
+
     // If this xbe is launched from C:/xboxdash.xbe we disable this installer option
     char target_path[MAX_PATH];
     nxGetCurrentXbeNtPath(target_path);
-    printf("target_path: %s\n", target_path);
     if (strcmp(target_path, "\\Device\\Harddisk0\\Partition2\\xboxdash.xbe") == 0) {
-        menu_items[2].callback = NULL;
+        menu_items[OFFLINE_INSTALL_LINE].callback = NULL;
     }
 
     menu_push(&menu);
@@ -96,12 +74,7 @@ void menu_install_dash_activate(void)
 
 static HANDLE downloader_thread_handle = NULL;
 static HANDLE downloader_semaphore = NULL;
-static void trigger_online_update()
-{
-    ReleaseSemaphore(downloader_semaphore, 1, NULL);
-}
-
-static void trigger_online_install()
+static void trigger_online_action()
 {
     ReleaseSemaphore(downloader_semaphore, 1, NULL);
 }
@@ -109,8 +82,8 @@ static void trigger_online_install()
 static inline void update_downloader_status(char *status, void *callback)
 {
     WaitForSingleObject(text_render_mutex, INFINITE);
-    menu_items[3].label = status;
-    menu_items[3].callback = callback;
+    menu_items[ONLINE_INSTALL_LINE].label = status;
+    menu_items[ONLINE_INSTALL_LINE].callback = callback;
     ReleaseMutex(text_render_mutex);
 }
 
@@ -130,7 +103,7 @@ static DWORD WINAPI downloader_update_thread(LPVOID lpThreadParameter)
         // Update the menu item to show the latest version
 
         snprintf(status_text, sizeof(status_text), "Download update (%s)?", latest_version);
-        update_downloader_status(status_text, trigger_online_update);
+        update_downloader_status(status_text, trigger_online_action);
 
         // Wait for user to trigger the download. We sleep for a short time to check if the user has aborted the update
         while (1) {
@@ -139,8 +112,7 @@ static DWORD WINAPI downloader_update_thread(LPVOID lpThreadParameter)
             }
             // Check if the menu has changed, if so we exit the thread
             if (menu_peak() != &menu) {
-                printf("Menu changed, exiting update thread.\n");
-                update_downloader_status("Check online for updates", check_online);
+                update_downloader_status(default_online_install_text, install_dashboard_from_online);
                 return 0;
             }
         }
@@ -157,7 +129,7 @@ static DWORD WINAPI downloader_update_thread(LPVOID lpThreadParameter)
                 update_downloader_status("Hash mismatch! - Aborting download", NULL);
             }
 
-            update_downloader_status("Update downloaded successfully! - Install?", trigger_online_install);
+            update_downloader_status("Update downloaded successfully! - Install?", trigger_online_action);
 
             // Wait for user to trigger the download. We sleep for a short time to check if the user has aborted the update
             while (1) {
@@ -166,9 +138,8 @@ static DWORD WINAPI downloader_update_thread(LPVOID lpThreadParameter)
                 }
                 // Check if the menu has changed, if so we exit the thread
                 if (menu_peak() != &menu) {
-                    printf("Menu changed, exiting update thread.\n");
                     free(mem);
-                    update_downloader_status("Check online for updates", check_online);
+                    update_downloader_status(default_online_install_text, install_dashboard_from_online);
                     return 0;
                 }
             }
@@ -188,27 +159,26 @@ static DWORD WINAPI downloader_update_thread(LPVOID lpThreadParameter)
             if (file_handle != INVALID_HANDLE_VALUE) {
                 DWORD bytes_written;
                 if (WriteFile(file_handle, downloaded_data, downloaded_size, &bytes_written, NULL)) {
-                    update_downloader_status("Update installed successfully!", check_online);
-                    printf("Btes written: %lu\n", bytes_written);
+                    update_downloader_status("Update installed successfully!", install_dashboard_from_online);
                 } else {
-                    update_downloader_status("Error writing update file", check_online);
+                    update_downloader_status("Error writing update file", install_dashboard_from_online);
                 }
                 CloseHandle(file_handle);
             } else {
-                update_downloader_status("Error opening file for writing", check_online);
+                update_downloader_status("Error opening file for writing", install_dashboard_from_online);
             }
             free(mem);
         } else {
-            update_downloader_status("Failed to download update", check_online);
+            update_downloader_status("Failed to download update", install_dashboard_from_online);
         }
 
     } else {
-        update_downloader_status("Failed to check for updates", check_online);
+        update_downloader_status("Failed to check for updates", install_dashboard_from_online);
     }
     return 0;
 }
 
-static void check_online(void)
+static void install_dashboard_from_online(void)
 {
     // Prevent starting the downloader thread multiple times
     if (downloader_thread_handle != NULL) {
@@ -233,4 +203,23 @@ static void check_online(void)
     update_downloader_status("Checking for updates...", NULL);
 
     ResumeThread(downloader_thread_handle);
+}
+
+static void install_dashboard_from_local(void)
+{
+    const char *src = "Q:\\default.xbe";
+    const char *dst = "C:\\xboxdash.xbe";
+    const char *bak = "C:\\xboxdash.xbe.bak";
+
+    SetFileAttributesA(dst, FILE_ATTRIBUTE_NORMAL);
+    SetFileAttributesA(bak, FILE_ATTRIBUTE_NORMAL);
+
+    // Create a backup
+    CopyFile(dst, bak, FALSE);
+
+    if (CopyFile(src, dst, FALSE)) {
+        menu_items[OFFLINE_INSTALL_LINE].label = "Installed successfully";
+    } else {
+        menu_items[OFFLINE_INSTALL_LINE].label = "Error installing";
+    }
 }
